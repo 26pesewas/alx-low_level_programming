2bb0469083032c386397b244499c875a90eee39c
Phyllis Laryea <naaakuokor@gmail.com>
2022-03-15 20:20:23 +0000

    prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main -  prints Holberton, followed by a new line
- * Return: Always 0 (Success)
+ * main -  prints _putchar, followed by a new line
+ * Return:  0 if successful
  */
+
 int main(void)
 {
-        _putchar("_");
+ 
+	_putchar('_');
         _putchar('p');
         _putchar('u');
         _putchar('t');
