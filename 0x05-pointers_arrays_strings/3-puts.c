ad9012aad71987211bded55ce9ac594a6ee3b517
26pesewas <naaakuokor@gmail.com>
2022-03-21 12:25:16 -0700

    draws a straight line in the terminal

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,13 +4,11 @@
 * _puts - prints a string to stdout
 * @str: pointer to the string to print
 */
+
 void _puts(char *str)
 {
-	int i;
+	while (*str)
+		putchar(*str++);
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
