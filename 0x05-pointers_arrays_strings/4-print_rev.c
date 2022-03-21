bf70f24ea3897e945b73efde3acdf5e291d5c941
26pesewas <naaakuokor@gmail.com>
2022-03-21 09:42:28 -0700

    prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
 * print_rev - prints a string in reverse, followed by a new line
 * @s: string to be printed
 */
-void print_rev(char *s)
+void print_rev(char *str)
 {
 	int i, j, len;
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
 	}
@@ -18,7 +19,7 @@ void print_rev(char *s)
 
 	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[j]);
+		_putchar(str[j]);
 	}
 
 	_putchar('\n');
