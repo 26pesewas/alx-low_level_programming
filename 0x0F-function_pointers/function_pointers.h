c7b3b1cdbb500f0739ce04ce6b57a4705cfebb42
26pesewas <naaakuokor@gmail.com>
2022-04-13 08:56:23 -0700

    task 3 correction

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
